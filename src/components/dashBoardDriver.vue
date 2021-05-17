93b51963187d683481b4a382063f110f2b297eb9
davidpoudel <davidpoudel19@gmail.com>
2021-05-17 12:14:18 +0545

    Frontend

---
+++ src/components/dashBoardDriver.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Welcome to the Driver's Dashboard</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/dashBoardManager.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Welcome to the Manager's Dashboard</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
